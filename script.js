5d61bd15e1d6eb31b6efaf58fe6174baac7eada2
Will Rongholt <will.rongholt@wellsaid.ai>
2025-03-03 14:12:08 -0600

    changed require to import

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-var requirejs = require('requirejs');
-
-requirejs.config({
-    //Pass the top-level main.js/index.js require
-    //function to requirejs so that node modules
-    //are loaded relative to the top-level JS file.
-    nodeRequire: require
-});
-require('dotenv').config()
+import 'dotenv/config';
 document.addEventListener("DOMContentLoaded", function () {
     var noise = new SimplexNoise();
     let isAnimating = false;
